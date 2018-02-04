e1a68081cba82609b75f05caf93a0a4010e27d44
Trung Dinh Quang <trungdq88@gmail.com>
2018-02-04 10:47:57 +0700

    Login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:message] = "Welcome back, #{user.username}!"
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:error] = 'Incorrect login information'
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash.now[:message] = 'You have logged out!'
+    render 'new'
   end
 end
