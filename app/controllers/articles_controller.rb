a938ef853153624b9d0a221455349fa6817c9339
Trung Dinh Quang <trungdq88@gmail.com>
2018-02-04 11:29:49 +0700

    Remove hardcode user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:message] = 'Article is created successfully!'
       redirect_to article_path(@article)
