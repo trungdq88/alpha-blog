763ecf74cece49453fd4af7048a3a9840080d208
Trung Dinh Quang <trungdq88@gmail.com>
2018-02-04 18:47:02 +0700

    Add ArticleCategories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
+  has_many :article_categories
+  has_many :articles, through: :article_categories
   validates :name, uniqueness: true, length: { minimum: 3, maximum: 25 }
 end
