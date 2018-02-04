e4bd5866d18dda1d84da6debca0f684fa24e17bc
Trung Dinh Quang <trungdq88@gmail.com>
2018-02-04 18:02:30 +0700

    more test

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,14 +1,24 @@
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
   test "create new categories" do
     get new_category_path
     assert_template 'categories/new'
-
     assert_difference 'Category.count', 1 do
       post categories_path, params: { category: { name: 'Sports' } }
       follow_redirect!
     end
-
     assert_template 'categories/index'
     assert_match 'Sports', response.body
   end
+
+  test "create new categories failed" do
+    get new_category_path
+    assert_template 'categories/new'
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: 'a' } }
+    end
+    assert_template 'categories/new'
+    assert_match 'Name is too short (minimum is 3 characters)', response.body
+  end
+
 end
